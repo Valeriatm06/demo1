03998ab210886efb872d1a0584ca9fa49b49b686
Valeriatm06 <valeria.tocarruncho@uptc.edu.co>
2024-08-20 17:32:14 -0500

    first commit

---
+++ demo1/src/main/java/co/edu/uptc/Main.java
@@ -0,0 +1,11 @@
+package co.edu.uptc;
+
+import co.edu.uptc.models.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("Valeriaaaa");
+        System.out.println(person.getName());
+    }
+}
